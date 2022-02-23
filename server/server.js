17e38de6d37dd1502a8e10b748038e8e6e4a8cae
carlie-gorrondona <carlie.gorrondona@gmail.com>
2022-02-23 15:21:24 -0600

    added endpoints so css will work with Heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './server/server.js'))
+})
+
+
 const port = process.env.PORT || 4006;
 
 app.listen(port, () => {
